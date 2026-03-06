7f8d4d3110e76c638feff7c5dfe357458c4df161
sb0769325-max <sb0769325@gmail.com>
2026-03-06 17:54:01 +0530

    Add files via upload

---
+++ PanelEx.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class PanelEx {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Panel Example");
+    JPanel panel = new JPanel();
+    panel.add(new JButton("Click Me"));
+    panel.add(new JLabel("Hello"));
+    frame.add(panel);
+    frame.setSize(300, 200);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+}
+
